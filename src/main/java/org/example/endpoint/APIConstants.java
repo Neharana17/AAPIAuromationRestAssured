f2dcb452d9a0214803331678a726c6ba11f3f504
Neharana17 <neharana17feb97@gmail.com>
2023-10-30 16:28:07 +0530

    Our automation Framwork 1 step

---
+++ src/main/java/org/example/endpoint/APIConstants.java
@@ -0,0 +1,10 @@
+package org.example.endpoint;
+
+public class APIConstants {
+
+    public static String BASE_URL= "https://restful-booker.herokuapp.com";
+
+    public static String Create_booking ="/booking";
+    public static String Update_booking ="/booking";
+
+}
---
+++ src/test/java/org/example/curd/TC_CreateBooking.java
@@ -0,0 +1,14 @@
+package org.example.curd;
+
+import org.testng.annotations.Test;
+
+public class TC_CreateBooking {
+    @Test
+    public  void testcreatebooking(){
+        System.out.println(" Hera be create booking for post request");
+    }
+
+    public  void testcreatebooking2(){
+
+    }
+}
